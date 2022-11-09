c60b8f576b26343e1d06e6f4e657ab1d74d1a7e0
sandra <t110aek002@ntut.org.tw>
2022-11-10 01:27:20 +0800

    landing, news, booking

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 111-1-midterm
+# 主題
+斑比山丘
+
+## 參考資料、圖片
+[斑比山丘Facebook](https://www.facebook.com/bambiland.yilan/) | 
+[斑比山丘Instagram](https://www.instagram.com/bambiland.yilan/) | 
+[斑比山丘線上預約](https://booking.menushop.tw/bambiland?fbclid=IwAR0XMCT_1Su9Sxgj12V53lA36Mqtmb2vqj47GBqscV-k1O9npzSNK833hpI)
+[玩遊世界菱距離](https://www.mitsubishi-motors.com.tw/BrandEvent/route.php?id=34) | 
+[美美子二店](https://travelwithlinlin.com/post-361669878/) | 
+[IG打卡](https://instagram.com/p/CgHtoMAPYed)
+
+其他圖片由友人提供
+
+## 組員名稱
+110AEK002 湯曉琪
+110AEK003 李鴻杰
---
+++ js/bambi.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.js-banner-carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        // autoplay: true,
+        fade: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        cssEase: 'linear',
+    });
+    $('.js-products-carousel').slick({
+        speed: 9000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        infinite: true,
+        dots: false,
+        arrows: false,
+    });
+    $('.back-to-top').click(function(){
+        console.log("omg");
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+    });
+});
